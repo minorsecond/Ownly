908fa9ac95cca9bd64a0b68c7dc52a6e89220a90
Ross Wardrup <minorsecond@gmail.com>
2020-08-26 06:06:31 -0500

    Add Qt5 info to CMakeLists.txt
    This project requires msys-installed mingw and Qt5 libraries.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "MainWindow.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int open_main_window(int argc, char** argv) {
+    QApplication app(argc, argv);
+    QMainWindow mainWindow;
+    QWidget widget;
+    Ui::MainWindow ui;
+    ui.setupUi(&mainWindow);
+
+    mainWindow.show();
+    return QApplication::exec();
+}
+
+int main(int argc, char** argv) {
+    open_main_window(argc, argv);
     return 0;
 }
