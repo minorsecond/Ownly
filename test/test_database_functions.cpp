aebdecb83ee367df3e6f79201e4ae40cb7437530
Ross Wardrup <minorsecond@gmail.com>
2020-08-28 04:51:08 -0500

    Test attributes of rows

--- test/test_database_functions.cpp
+++ test/test_database_functions.cpp
@@ -22,7 +22,6 @@ TEST_CASE("Init DB", "[DB Creation]") {
     REQUIRE( exists(file_name) == true);
     REQUIRE( table_names.at(0) == "items");
     std::remove(file_name.c_str());
-    std::cout << "Init DB test ran." << std::endl;
 }
 
 TEST_CASE( "DB Write", "[DB Write]" ) {
@@ -30,15 +29,23 @@ TEST_CASE( "DB Write", "[DB Write]" ) {
     Storage storage = initStorage(file_name);
     db.writeDbToDisk(storage);
 
-    Item item{-1, "Lord of the Rings - The Two Towers", "Book", 1998,
+    Item item{-1, "The Silmarillion", "Book", 1998,
               3, 7, 8.99, 1, true,
-              "This is absoluately my favorite book."};
+              "This is absolutely my favorite Tolkien book."};
     int firstInsertedId = storage.insert(item);
 
     Item secondItem{-1, "HF Ham Radio", "Electronics", 2001, 8,
                     14, 599.99, 1, true, "Great radio."};
     int secondInsertedId = storage.insert(secondItem);
 
+    Item book = storage.get<Item>(firstInsertedId);
+    Item radio = storage.get<Item>(secondInsertedId);
+
     REQUIRE ( firstInsertedId == 1 );
     REQUIRE ( secondInsertedId == 2 );
+    REQUIRE( book.itemName == "The Silmarillion" );
+    REQUIRE( book.purchasePrice == 8.99 );
+    REQUIRE( book.notes == "This is absolutely my favorite Tolkien book." );
+
+    std::remove(file_name.c_str());
 }
