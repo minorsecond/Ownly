59369e9444d9ea8caad63bb4b5d6fa0f34062363
Ross Wardrup <minorsecond@gmail.com>
2020-08-28 07:59:36 -0500

    Get input values

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,32 @@ MainWindow::MainWindow(QWidget *parent) {
     ui.setupUi(this);
     this->setFixedSize(800, 537);
 
-    std::string item_name;
-    std::string item_category;
-    double item_price;
-    int purchaseYear;
-    int purchaseMonth;
-    int purchaseDay;
-    int itemCount;
-    bool usedInLastSixMonths;
-    std::string notes;
-
     connect(ui.dbSubmitButton, SIGNAL(clicked()), this, SLOT(clicked_submit()));
 }
 
 void MainWindow::clicked_submit(){
     std::cout << "Clicked dbSubmitButton" << std::endl;
+
+    std::string item_name = ui.ItemName->text().toUtf8().constData();
+    std::string item_category = ui.ItemCategory->currentText().toUtf8().constData();
+    std::string item_price_string = ui.ItemPurchasePrice->text().toUtf8().constData();
+    double item_price;
+    std::string purchase_date = ui.ItemPurchaseDate->text().toUtf8().constData();
+    int item_count = std::stoi(ui.ItemCount->text().toUtf8().constData());
+    bool usedInLastSixMonths = ui.ItemUsedInLastSixMonths->isChecked();
+    std::string notes = ui.ItemCount->text().toUtf8().constData();
+
+    if (item_price_string.empty())
+        item_price = 0.00;
+    else
+        item_price = std::stod(item_price_string);
+
+    std::cout << "Item price: " << item_price << std::endl;
+
+    //Item item{
+    //    -1,
+    //    ui.ItemName->trUtf8().constData();
+    //};
 }
 
 int main(int argc, char** argv) {
