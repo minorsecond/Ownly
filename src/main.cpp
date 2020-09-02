f7c243fdf288d51d759213642a216bba9a53817f
Ross Wardrup <minorsecond@gmail.com>
2020-09-02 11:53:01 -0500

    Check for empty item count

--- src/main.cpp
+++ src/main.cpp
@@ -57,12 +57,19 @@ void MainWindow::clicked_submit(){
     std::string item_category = ui.ItemCategory->currentText().toUtf8().constData();
     std::string item_price_string = ui.ItemPurchasePrice->text().toUtf8().constData();
     double item_price;
-    int item_count = std::stoi(ui.ItemCount->text().toUtf8().constData());
+    std::string item_count_str = ui.ItemCount->text().toUtf8().constData();
     bool usedInLastSixMonths = ui.ItemUsedInLastSixMonths->isChecked();
     std::string notes = ui.ItemNotes->toPlainText().toUtf8().constData();
     int purchase_year = ui.ItemPurchaseDate->date().year();
     int purchase_month = ui.ItemPurchaseDate->date().month();
     int purchase_day = ui.ItemPurchaseDate->date().day();
+
+    // Handle blank item counts that may happen if user uses the new item button.
+    int item_count;
+    if (!item_count_str.empty())
+        item_count = std::stoi(ui.ItemCount->text().toUtf8().constData());
+    else
+        item_count = 0;
 
     if (item_price_string.empty())
         item_price = 0.00;
