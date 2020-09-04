a568621192452ef656f2a71e56c4a4d2f4b2d127
Ross Wardrup <minorsecond@gmail.com>
2020-09-04 15:30:21 -0500

    Add documentation

--- src/exporters.h
+++ src/exporters.h
@@ -8,6 +8,9 @@
 #include "Database.h"
 
 class exporters {
+    /*
+     * Methods for exporting from database file to flat files.
+     */
 public:
     static void to_csv(const std::vector<Item>& items, const std::string& path);
 };
